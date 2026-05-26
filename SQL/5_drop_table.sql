b646f961f65b9e714670bc35f378473208a34456
kamar-ben <120564867+kamar-bit@users.noreply.github.com>
2026-05-26 11:02:39 +0200

    Create 5_drop_table.sql

---
+++ SQL/5_drop_table.sql
@@ -0,0 +1,20 @@
+-- =========================================================
+-- Suppression des tables
+-- =========================================================
+
+DROP TABLE IF EXISTS accusation CASCADE;
+DROP TABLE IF EXISTS arrestation CASCADE;
+DROP TABLE IF EXISTS interrogatoire CASCADE;
+DROP TABLE IF EXISTS lier_preuve_suspect CASCADE;
+DROP TABLE IF EXISTS preuve CASCADE;
+DROP TABLE IF EXISTS associer_lieu_affaire CASCADE;
+DROP TABLE IF EXISTS lieu_crime CASCADE;
+DROP TABLE IF EXISTS temoigner_dans CASCADE;
+DROP TABLE IF EXISTS temoin CASCADE;
+DROP TABLE IF EXISTS affecter CASCADE;
+DROP TABLE IF EXISTS affaire CASCADE;
+DROP TABLE IF EXISTS victime CASCADE;
+DROP TABLE IF EXISTS suspect CASCADE;
+DROP TABLE IF EXISTS enqueteur CASCADE;
+DROP TABLE IF EXISTS grade CASCADE;
+DROP TABLE IF EXISTS personne CASCADE;
